4ce832dcf06c6e05507970ad594d2f23b68b4610
Freyn, Lucas <lukefreyn@gmail.com>
2026-09-17 12:06:36 -0400

    Add files via upload

---
+++ monster project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // This is my pet monster!!
+        Console.WriteLine(" .-.");
+        Console.WriteLine("(o o)");
+        Console.WriteLine("| O |");
+        Console.WriteLine("|   |");
+        Console.WriteLine("'~~~'");
+        Console.WriteLine("Lucas Cael Freyn The Creature");
+    }
+}
