f3271ba06946eb08c1960474b2947bb7342a38e5
Freyn, Lucas <lukefreyn@gmail.com>
2026-09-17 12:04:35 -0400

    Add files via upload

---
+++ triangle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Prompt the user to enter the base of the triangle
+        Console.Write("Enter the base of the triangle: ");
+        double baseOfTriangle = Convert.ToDouble(Console.ReadLine());
+
+        // Prompt the user to enter the height of the triangle
+        Console.Write("Enter the height of the triangle: ");
+        double heightOfTriangle = Convert.ToDouble(Console.ReadLine());
+
+        // Calculate the area of the triangle
+        double area = 0.5 * baseOfTriangle * heightOfTriangle;
+
+        // Display the result
+        Console.WriteLine($"The area of the triangle is: {area}");
+    }
+}
